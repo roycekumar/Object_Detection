a8d5826d01707c11d0bc14cda84b5d62e2bc0e4e
Royce Kumar <69853766+roycekumar@users.noreply.github.com>
2020-11-11 16:52:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 img="";
+status="";
 function preload(){
-    img=loadImage('dog_cat.jpg');
+    img=loadImage('lm.jpg');
 }
 function setup(){
     canvas=createCanvas(640,420);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status:Detecting Objects";
 }
 function draw(){
     image(img,0,0,640,420);
-    fill("#FF0000");
-    text("Dog", 45,85);
-    textSize(24);
-    noFill();
-    stroke("#FF0000");;
-    rect(30,60,450,350);
-    stroke("#4287f5");
-    fill("#4287f5");
-    text("Cat", 240,110);
-
-    noFill();
-    rect(230,70,350,320);
 }
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+  }
+  
